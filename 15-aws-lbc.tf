d80e8a28c9097d18289be669a0a9af776c1d70b7
harkanni <harkanzyempire001@gmail.com>
2026-09-23 18:14:46 +0100

    Fix IAM policy attachment reference for AWS Load Balancer Controller

--- 15-aws-lbc.tf
+++ 15-aws-lbc.tf
@@ -25,7 +25,7 @@ resource "aws_iam_policy" "aws_lbc" {
 }
 
 resource "aws_iam_role_policy_attachment" "aws_lbc" {
-  policy_arn = aws_iam_role.aws_lbc.arn
+  policy_arn = aws_iam_policy.aws_lbc.arn
   role       = aws_iam_role.aws_lbc.name
 }
 
